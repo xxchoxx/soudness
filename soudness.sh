585ded8c8d38a0135b56c2f46cccfb58b1614348
xcho <88242258+xxchoxx@users.noreply.github.com>
2025-03-27 20:02:22 +0700

    Update soudness.sh

--- soudness.sh
+++ soudness.sh
@@ -13,15 +13,20 @@ sleep 1
 
 sudo apt update && sudo apt upgrade -y
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source $HOME/.cargo/env
 
 rustc --version
+
 cargo --version
 
 echo 'source $HOME/.cargo/env' >> ~/.bashrc
+
 source ~/.bashrc
 
+curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/soundnessup/install | bash
+
 soundnessup install
+
 soundness-cli generate-key --name my-key
